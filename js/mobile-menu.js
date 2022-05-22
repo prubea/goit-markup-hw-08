c7dab3c40b247d126225ba54e58e656f098a0e74
Beata Pruszczyk <prubea@gmail.com>
2022-05-22 09:24:23 +0200

    menu mobilne

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,13 +1,14 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
-    const mobileBtnClose = document.querySelector("[data-menu-close]");
-  
-    menuBtnRef.addEventListener("click", () => {
-      mobileMenuRef.classList.toggle("is-open");
-    })
+  const refs = {
+    openMenuBtn: document.querySelector('[data-mobile-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-mobile-menu-close]'),
+    modal: document.querySelector('[data-mobile-menu]'),
+  };
 
-    mobileBtnClose.addEventListener('click', () => {
-      mobileMenuRef.classList.toggle("is-open");
-    });
-  })()
+  refs.openMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
